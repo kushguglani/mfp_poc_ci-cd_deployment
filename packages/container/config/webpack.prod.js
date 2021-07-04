5f2cce7dc4dce51164be72be80816db6c4299c8a
DzinnApp <dzinnapp@gmail.com>
2021-07-04 13:40:32 +0530

    Created conatiner deploy script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { merge } = require('webpack-merge');
+
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json')
+const productionDomain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+	mode: 'production',
+	output: {
+		filename: '[name].[contenthash].js',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+		new ModuleFederationPlugin({
+			name: 'conatiner',
+			remotes: {
+				marketing: `marketing@${productionDomain}/marketing/remoteEntry.js`,
+			},
+			shared: packageJson.dependencies
+		})
+	]
+}
+
+module.exports = merge(commonConfig, prodConfig);
