cce2908d815522bb51924ac2c48a5139e07f0bf5
DzinnApp <dzinnapp@gmail.com>
2021-07-05 21:57:11 +0530

    css issue with random calss name

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -3,12 +3,18 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from "./component/MarketingApp";
 import Header from './component/header'
-console.log("--------Container--------");
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'co'
+})
 
 
 export default () => (
 	<BrowserRouter>
-		<Header />
-		<MarketingApp />
+		<StylesProvider generateClassName={generateClassName}>
+			<Header />
+			<MarketingApp />
+		</StylesProvider>
 	</BrowserRouter>
 )
