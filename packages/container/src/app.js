1bc218be2d3a5977337aeb2e4fe7608422ad0e4e
DzinnApp <dzinnapp@gmail.com>
2021-07-05 11:05:27 +0530

    build script in package333

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -3,4 +3,7 @@ import React from 'react';
 // import UseRefExample from './component/useRefExample'; 
 import MarketingApp from "./component/MarketingApp";
 
+console.log("--------Container--------");
+
+
 export default () => (<MarketingApp/>)
