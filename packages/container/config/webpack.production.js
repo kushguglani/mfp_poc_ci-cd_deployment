e0fcf2d76426fed7ec6c51d941962710e49aa5f4
DzinnApp <dzinnapp@gmail.com>
2021-07-05 11:47:11 +0530

    build script in package4

--- packages/container/config/webpack.production.js
+++ packages/container/config/webpack.production.js
@@ -15,7 +15,7 @@ const prodConfig = {
 		new ModuleFederationPlugin({
 			name: 'container',
 			remotes: {
-				marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+				marketing: `marketing@${domain}/marketing/abc1234567/latest/remoteEntry.js`,
 			},
 			shared: packageJson.dependencies,
 		}),
